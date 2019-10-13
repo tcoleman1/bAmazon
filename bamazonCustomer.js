f34f3f7e03bbb65fcd15ce03ce2db4cb90a1a658
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-10-13 10:34:46 -0400

    I can now enter product ID on startup of app w/o pressing a key.Now able to hit the if block for insufficient quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,43 +19,64 @@ function getItems() {
     if (err) throw err;
     for (var i=0; i<res.length; i++){
         // console.log('Below you will find the list of items left' + res[i].item_id + '|' + res[i].product_name);
-        console.log('Below you will find the list of items in our inventory:' ,
-        'Item Id:' + `${res[i].item_id}
-     ${res[i].product_name}
-        ${res[i].price}`);
+        console.log(`---------------------------
+      PRODUCT ID:      ${res[i].item_id}
+     PRODUCT NAME:    ${res[i].product_name}
+     PRODUCT PRICE:   ${res[i].price}`);
     }
+    promptUser();
 })
 // connection.end()
 
 }
-promptUser();
+
 
 function promptUser () {
     inquirer
     .prompt([
         {
-        name: 'product ID',
+        name: 'productID',
         type: 'input',
         message: "Please Enter the ID of the product you'd like to buy",
         
         },
 
         {
-            name: 'Product Quantity',
-            type: 'quantityChoice',
+            name: 'productQuantity',
+            type: 'input',
             message: "How many units of this product would you like to buy?"
         }
 
     ])
  
-    .then(function(item_id){
-            connection.query("SELECT * from products WHERE item_id=?", option.item_id, function(err,res){
+    .then(function(option){
+       // console.log(option)
+        
+        console.log("We've reached this point")
+          ///  connection.query("SELECT * from products WHERE item_id=${option.productID}", function(res,err){
+              connection.query("SELECT * from products WHERE item_id=?", [
+
+                    option.productID
+              ], 
+                
+            function(err,res){
+                console.log(res)
+                console.log(err)
+               console.log('This is my prod id:' + option.productID)
                 if(err) throw err
-                else if(item_id.quantityChoice > res[i].stock_quantity){
+                console.log(res[0].stock_quantity)
+                if(res[0].stock_quantity > parseInt(option.productQuantity)){
                     console.log("Insufficient Quantity, please try another product")
                 }
-               else if(item_id.quantityChoice < res[i].stock_quantity){
-                    console.log("Ok the price of this item is:" + res[i].price)
+               else if(option.quantityChoice <= res[0].stock_quantity){
+                   let totalCost = res[0].price * option.productQuantity
+                 // console.log("Ok the price of this item is:" + "$"+ totalCost +
+                   // ".Thank you for shopping with us");
+                    connection.query(`UPDATE products SET stock_quantity = "${res[0].stock_quantity - parseInt(option.productQuantity)}" WHERE item_id=${parseInt(option.productID)}`, function(err,res){
+
+                     if (err) throw err;
+                    console.log("Ok the price of this item is:" + "$"+ totalCost + ".Thank you for shopping with us");
+                    })
                }
             })
         })
